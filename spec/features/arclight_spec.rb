4703b58588b1a0dbbc7b91f5fa27543b13f85649
Sean Aery <sean.aery@duke.edu>
2020-01-14 15:32:59 -0500

    Add support for feature testing with Capybara & webdrivers; revise rspec setup to resemble ArcLight core.

---
+++ spec/features/arclight_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Replicated from ArcLight core
+
+require 'spec_helper'
+
+RSpec.describe 'Arclight', type: :feature do
+  it 'navigates to homepage' do
+    visit '/'
+    expect(page).to have_css 'h1', text: 'Archival Collections at Institution'
+  end
+end
