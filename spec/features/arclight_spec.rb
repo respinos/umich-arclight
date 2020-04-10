15a0f3ae6a74fa31a7c530d5261fc504b4eb07dc
md169@duke.edu <md169@duke.edu>
2020-04-10 08:41:37 -0400

    Arc 119 update breadcrumb presentation

--- spec/features/arclight_spec.rb
+++ spec/features/arclight_spec.rb
@@ -5,9 +5,9 @@ require 'spec_helper'
 # Removed this spec for now - we can revisit once there is actual
 # content for the homepage
 #
-RSpec.describe 'Arclight', type: :feature do
-  xit 'navigates to homepage' do
-    visit '/'
-    expect(page).to have_css 'h1', text: 'Archival Collections at Duke'
-  end
-end
+# RSpec.describe 'Arclight', type: :feature do
+#   xit 'navigates to homepage' do
+#     visit '/'
+#     expect(page).to have_css 'h1', text: 'Archival Collections at Duke'
+#   end
+# end
