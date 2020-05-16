e35ea8b7c8924410e7a90c9118d1bcd0d1e65c22
Sean Aery <sean.aery@duke.edu>
2020-05-15 21:25:11 -0400

    Resolve rubocop violations.

--- spec/controllers/index_finding_aids_controller_spec.rb
+++ spec/controllers/index_finding_aids_controller_spec.rb
@@ -1,54 +1,50 @@
 RSpec.describe IndexFindingAidsController, type: :controller do
+  describe '#create' do
+    let(:token) { SecureRandom.hex(8) }
+    let(:payload) do
+      {
+        'commits' => [
+          {
+            'id' => 'b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327',
+            'added' => ['README.md'],
+            'modified' => ['.gitlab-ci.yml'],
+            'removed' => []
+          },
+          {
+            'id' => 'da1560886d4f094c3e6c9ef40349f7d38b5d27d7',
+            'added' => [],
+            'modified' => ['ead/rubenstein/rushbenjaminandjulia.xml'],
+            'removed' => []
+          }
+        ]
+      }
+    end
 
- describe "#create" do
+    before do
+      allow(DulArclight).to receive(:gitlab_token) { token }
+      allow(controller).to receive(:update_finding_aid_data).and_return(nil)
+    end
 
-   let(:token) { SecureRandom.hex(8) }
-   let(:payload) do
-{
-  "commits"=>[
-    {
-      "id"=>"b6568db1bc1dcd7f8b4d5a946b0b91f9dacd7327",
-      "added"=>["README.md"],
-      "modified"=>[".gitlab-ci.yml"],
-      "removed"=>[]
-    },
-    {
-      "id"=>"da1560886d4f094c3e6c9ef40349f7d38b5d27d7",
-      "added"=>[],
-      "modified"=>["ead/rubenstein/rushbenjaminandjulia.xml"],
-      "removed"=>[]
-    }
-  ]
-}
-   end
+    it 'requires a Gitlab token header' do
+      post :create, body: payload.to_json, as: :json
+      expect(response).to have_http_status(:unauthorized)
+    end
 
-   before do
-     allow(DulArclight).to receive(:gitlab_token) { token }
-     allow(controller).to receive(:update_finding_aid_data) { nil }
-   end
+    it 'require a Gitlab event header' do
+      request.headers['X-Gitlab-Token'] = token
+      post :create, body: payload.to_json, as: :json
+      expect(response).to have_http_status(:forbidden)
+    end
 
-   it "requires a Gitlab token header" do
-     post :create, body: payload.to_json, as: :json
-     expect(response).to have_http_status(:unauthorized)
-   end
-
-   it "require a Gitlab event header" do
-     request.headers['X-Gitlab-Token'] = token
-     post :create, body: payload.to_json, as: :json
-     expect(response).to have_http_status(:forbidden)
-   end
-
-   specify {
-     expect(IndexFindingAidJob)
-       .to receive(:perform_later)
-             .with("/data/ead/rubenstein/rushbenjaminandjulia.xml", "rubenstein")
-             .and_call_original
-     request.headers['X-Gitlab-Token'] = token
-     request.headers['X-Gitlab-Event'] = 'Push Hook'
-     post :create, body: payload.to_json, as: :json
-     expect(response).to have_http_status(:accepted)
-   }
-
- end
-
+    specify do
+      expect(IndexFindingAidJob)
+        .to receive(:perform_later)
+        .with('/data/ead/rubenstein/rushbenjaminandjulia.xml', 'rubenstein')
+        .and_call_original
+      request.headers['X-Gitlab-Token'] = token
+      request.headers['X-Gitlab-Event'] = 'Push Hook'
+      post :create, body: payload.to_json, as: :json
+      expect(response).to have_http_status(:accepted)
+    end
+  end
 end
--- spec/routing/index_finding_aids_routing_spec.rb
+++ spec/routing/index_finding_aids_routing_spec.rb
@@ -1,8 +1,6 @@
-RSpec.describe "index_finding_aids routing", type: :routing do
-
-  specify {
+RSpec.describe 'index_finding_aids routing', type: :routing do
+  specify do
     expect(post: '/index_finding_aids')
       .to route_to(controller: 'index_finding_aids', action: 'create')
-  }
-
+  end
 end
