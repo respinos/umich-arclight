607c25d4958bbf941ee6f6439b24f35ffd6e9c79
Sean Aery <sean.aery@duke.edu>
2020-06-18 15:23:09 -0400

    Various accessibility fixes: heading levels, aria labels, color contrast, landmarks. Closes the following: * ARC-213 - homepage heading level progression * ARC-217 - collection info button label * ARC-220 - container info contrast * ARC-221 - embed iframe titles * ARC-227 - restriction warning <dl> wrap * ARC-231 - landmark regions for sidebar navs

--- spec/features/arclight_spec.rb
+++ spec/features/arclight_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe 'DUL Arclight Pages', type: :feature do
   it 'navigates to homepage' do
     visit '/'
     expect(page).to have_css 'h1', text: 'Find Archival Materials'
-    expect(page).to have_css 'h3', text: 'About This Site'
-    expect(page).to have_css 'h3', text: 'Find More in the Catalog'
-    expect(page).to have_css 'h3', text: 'Ask a Librarian'
+    expect(page).to have_css 'h2', text: 'About This Site'
+    expect(page).to have_css 'h2', text: 'Find More in the Catalog'
+    expect(page).to have_css 'h2', text: 'Ask a Librarian'
   end
 
   it 'navigates to advanced search' do
