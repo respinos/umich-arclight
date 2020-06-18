b5fa3bc05205ccbab66f668f10b61689651d7201
David Chandek-Stark <david.chandek.stark@duke.edu>
2020-06-18 12:48:10 -0400

    Fixes uninitialized constant error observed on dev server

--- app/jobs/build_suggest_job.rb
+++ app/jobs/build_suggest_job.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class BuildSuggestJob < ApplicationJob
 
   queue_as :index
@@ -6,7 +8,7 @@ class BuildSuggestJob < ApplicationJob
     # Not using Blacklight.default_index.connection for greater control
     uri = URI("#{ENV['SOLR_URL']}/suggest?suggest.build=true")
 
-    Net::HTTP.start(uri.host, uri.port) do |http|
+    ::Net::HTTP.start(uri.host, uri.port) do |http|
       http.read_timeout = 600
       res = http.request_get(uri.path, {'Accept'=>'application/json'})
       res.value # raises exception if not 2XX status
