acc54f502d426fad547912ca35edb92dc1de3969
Sean Aery <sean.aery@duke.edu>
2020-12-07 10:21:16 -0500

    Bugfix: don't singularize VHS extent (e.g. 2 VHS to 1 VH). Closes ARC-319.

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -22,4 +22,5 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.singular(/^(.+) Feet/, '\1 Foot')
   inflect.irregular 'leaf', 'leaves'
   inflect.irregular 'Leaf', 'Leaves'
+  inflect.uncountable %w( VHS )
 end
