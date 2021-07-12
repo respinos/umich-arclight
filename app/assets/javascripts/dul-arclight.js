c022759206bac902b2fd64b2cf19517950534813
Laurin Penland <mlp60@duke.edu>
2021-07-12 16:32:38 -0400

    Add reset/delete button for search box. Closes ARC-359.

--- app/assets/javascripts/dul-arclight.js
+++ app/assets/javascripts/dul-arclight.js
@@ -65,6 +65,33 @@ Blacklight.onLoad(function () {
     $('.search-query-form')[0].submit();
   });
 
+
+  /* Do not display reset button when the page is first loaded. */
+  if (!$('input#q').val()) {
+    $('#reset').css('display', 'none');
+  }
+
+  /* If the input value has a length of at least 1, display the */
+  /* reset button. */
+  $('input#q').keyup(function() {
+    inputValue = $('input#q').val().length;
+    if (inputValue > 0) {
+      $('#reset').css('display', 'inline');
+    }
+    /* If the user manually deletes the input value, hide the button. */
+    if (inputValue == 0) {
+      $('#reset').css('display', 'none');
+    }
+  });
+
+  /* When the user clicks on the reset button, clear the value. This */
+  /* behaves slightly different than a standard reset button, because it */
+  /* clears the value even after the form has been submitted. */
+  $('#reset').click(function() {
+    $('.tt-input').val('');
+    $('#reset').css('display', 'none');
+  });
+
   /* ================= */
   /* MASTHEAD BEHAVIOR */
   /* ================= */
