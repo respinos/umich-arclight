9db1dc1b2dda61833c0e148e08f0cf5d2ab22abb
Anthony Thomas <esty@umich.edu>
2022-01-11 13:27:20 -0500

    Basically add back the repositories page

---
+++ app/controllers/repositories_controller.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Arclight
+  # Controller for our /repositories index page
+  class RepositoriesController < ApplicationController
+    def index
+      @repositories = Arclight::Repository.all
+      load_collection_counts
+    end
+
+    def show
+      @repository = Arclight::Repository.find_by!(slug: params[:id])
+      search_service = Blacklight.repository_class.new(blacklight_config)
+      @response = search_service.search(
+        q: "level_sim:Collection repository_sim:\"#{@repository.name}\"",
+        rows: 100
+      )
+      @collections = @response.documents
+    end
+
+    private
+
+    def load_collection_counts
+      counts = fetch_collection_counts
+      @repositories.each do |repository|
+        repository.collection_count = counts[repository.name] || 0
+      end
+    end
+
+    def fetch_collection_counts
+      search_service = Blacklight.repository_class.new(blacklight_config)
+      results = search_service.search(
+        q: 'level_sim:Collection',
+        'facet.field': 'repository_sim',
+        rows: 0
+      )
+      Hash[*results.facet_fields['repository_sim']]
+    end
+  end
+end
