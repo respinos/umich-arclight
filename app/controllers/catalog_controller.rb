40a5d955bf63bc48ac4f0982a4376dfbe2b9d80f
Anthony Thomas <esty@umich.edu>
2022-06-17 12:19:22 -0400

    Hide bookmark controls and "log in" link
    If nobody specifically requests this functionality, then we don't need
    to support it...

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -71,13 +71,13 @@ class CatalogController < ApplicationController
     config.show.display_type_field = 'level_ssm'
     # config.show.thumbnail_field = 'thumbnail_path_ss'
 
-    config.add_results_document_tool(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
+    # config.add_results_document_tool(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
 
     config.add_results_collection_tool(:sort_widget)
     config.add_results_collection_tool(:per_page_widget)
     config.add_results_collection_tool(:view_type_group)
 
-    config.add_show_tools_partial(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
+    # config.add_show_tools_partial(:bookmark, partial: 'bookmark_control', if: :render_bookmarks_control?)
     config.add_show_tools_partial(:email, callback: :email_action, validator: :validate_email_params)
     # DUL CUSTOMIZATION: Add CSV export especially for bookmarks
     config.add_show_tools_partial(:export_csv, partial: 'export_csv')
