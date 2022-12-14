6313d83a014c939957a8658a4810b218b0f0c1d1
Roger Espinosa <roger@umich.edu>
2022-12-14 17:15:02 -0500

    DCP2-386 + DCP2-446 (#55)
    * standarize title to local conventions
    
    * move local helper to local module

---
+++ app/helpers/um_arclight_helper.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+# Helper methods specific to UM ArcLight
+# ---------------------------------------
+module UmArclightHelper
+
+  # Render an html <title> appropriate string for a set of search parameters, based on local requirements
+  # @param [ActionController::Parameters] params
+  # @return [String]
+  # adapated from https://github.com/projectblacklight/blacklight/blob/main/app/helpers/blacklight/catalog_helper_behavior.rb#L207
+  def render_search_as_breadcrumbs_to_page_title(search_state_or_params)
+    search_state = if search_state_or_params.is_a? Blacklight::SearchState
+                     search_state_or_params
+                   else
+                     controller.search_state_class.new(params, blacklight_config, self)
+                   end
+
+    constraints = []
+    suffixes = []
+    prefix = t('blacklight.search.page_title.prefix')
+    add_prefix = true
+
+    if search_state.query_param.present?
+      q_label = label_for_search_field(search_state.search_field.key) unless search_state.search_field&.key.blank? || default_search_field?(search_state.search_field.key)
+
+      constraints += if q_label.present?
+                       [t('blacklight.search.page_title.constraint', label: q_label, value: search_state.query_param)]
+                     else
+                       [search_state.query_param]
+                     end
+    end
+
+    if search_state.filters.any?
+      repository = collection = nil
+      has_level_collection = false
+
+      search_state.filters.each do |filter|
+        if filter.key == 'repository_sim'
+          repository = filter.values.first
+        elsif filter.key == 'collection_sim'
+          collection = filter.values.first
+        elsif filter.key == 'level_sim'
+          has_level_collection = filter.values.first == 'Collection'
+        else
+          constraints << render_search_to_page_title_filter(filter.key, filter.values)
+        end
+      end
+      suffixes << repository unless repository.nil?
+      if collection.nil?
+        if has_level_collection and suffixes.present?
+          suffixes.unshift "Collections"
+        end
+      else
+        suffixes.unshift collection
+        add_prefix = constraints.present?
+      end
+    end
+
+    title = []
+    unless constraints.empty?
+      title += [ constraints.join(' / ') ]
+    end
+    unless suffixes.empty?
+      title << '-' unless title.empty?
+      title << suffixes.join(' - ')
+    end
+    title.unshift prefix if ( add_prefix )
+    title = title.join(' ')
+  end
+
+end
