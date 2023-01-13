f4eae9b57115a4fec461ce3dc57b2cd25f010b1d
Roger Espinosa <roger@umich.edu>
2023-01-13 16:57:08 -0500

    add html and pdf routes

--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,12 @@ Rails.application.routes.draw do
   get '/catalog/:id/xml', action: 'ead_download', controller: 'catalog', as: 'ead_download',
                           constraints: { id: collection_slug_constraint }
 
+  get '/catalog/:id/html', action: 'html_download', controller: 'catalog', as: 'html_download',
+                          constraints: { id: collection_slug_constraint }
+
+  get '/catalog/:id/pdf', action: 'pdf_download', controller: 'catalog', as: 'pdf_download',
+                          constraints: { id: collection_slug_constraint }
+
   # DUL CUSTOMIZATION: Render a sitemap on-the-fly from a query (if configured)
   get '/custom_sitemaps/:id', controller: 'custom_sitemaps', action: 'index', defaults: { format: 'xml' },
                               constraints: ->(request) { CUSTOM_SITEMAP_CONFIG.key?(request.params[:id]) }
