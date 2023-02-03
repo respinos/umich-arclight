58abd37bd565bad38c5ab5077b7459594e5e404c
Roger Espinosa <roger@umich.edu>
2023-02-03 16:16:02 -0500

    base interesting-ness on blacklight_config.component_fields

--- app/helpers/um_arclight_helper.rb
+++ app/helpers/um_arclight_helper.rb
@@ -64,4 +64,11 @@ module UmArclightHelper
     title.unshift prefix if add_prefix
     _title = title.join(' ')
   end
+
+  SKIPPABLE_KEYS = ['containers', 'physdesc_tesim', 'creators_ssim', 'abstract_teism', 'scopecontent_tesim']
+  def is_interesting_component?(document)
+    (blacklight_config.component_fields.keys.find do |key|
+      (SKIPPABLE_KEYS.exclude?(key) && document.fetch(key, nil).present?)
+    end) || document.is_linkable?
+  end
 end
