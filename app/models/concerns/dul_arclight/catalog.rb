d6514bc866d420f07d682e09bdc909900ff2a028
Roger Espinosa <roger@umich.edu>
2023-02-14 16:29:46 -0500

    fix abstract_tesim

--- app/models/concerns/dul_arclight/catalog.rb
+++ app/models/concerns/dul_arclight/catalog.rb
@@ -59,7 +59,7 @@ module DulArclight
       # replace m-arclight-placeholder with current asset styles/scripts
       self.response_body = Enumerator.new do |output|
         File.foreach(download_helper.html_file_path) do |line|
-          if line.index('<m-arclight-placeholder></m-arclight-placeholder>')
+          if line.index('<base id="placeholder">')
             output << helpers.stylesheet_link_tag('application', media: 'all')
             output << helpers.javascript_include_tag('application')
             output << helpers.csrf_meta_tags
